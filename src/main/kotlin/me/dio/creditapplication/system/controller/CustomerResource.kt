2e7118bfc9539c068b8a0e21262efb26423d52d2
Felipe Guizarra <felipeguizarra@gmail.com>
2023-07-29 18:44:56 -0300

    corrigindo nome da URI de customer

--- src/main/kotlin/me/dio/creditapplication/system/controller/CustomerResource.kt
+++ src/main/kotlin/me/dio/creditapplication/system/controller/CustomerResource.kt
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/costumer")
+@RequestMapping("/api/customer")
 class CustomerResource(
     private val customerService: CustomerService
 ) {
